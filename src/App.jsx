33337ce8ebcd1e3b8d5889e69564bbefa17f6055
faizurrehman <mirzafaiz506@gmail.com>
2025-10-24 19:21:49 +0530

    made nav bar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "./conponents/Nav";
+
+function App() {
+  return <div className="w-full bg-zinc-900 h-screen text-white">
+    <Nav/>
+  </div>;
+}
+
+export default App;
